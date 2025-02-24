329fc3276e176dae30ceb0057a40316f6ac1bd74
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-25 00:26:21 +0600

    Home page designed...

---
+++ scripts/payoo.js
@@ -0,0 +1,11 @@
+
+document.getElementById("btn-submit").addEventListener("click", function(event){
+    event.preventDefault();
+
+    let accountNumber = document.getElementById("account-number").value;
+    
+    let pin = document.getElementById("pin").value;
+    let convertedPin = parseInt(pin);
+    
+    
+})
