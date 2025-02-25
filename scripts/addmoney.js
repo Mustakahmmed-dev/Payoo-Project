6ba8eedf7a26f8f031ced0f1255c9f6da6b7e647
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-25 15:36:47 +0600

    JS function to adding money to main balance is done

---
+++ scripts/addmoney.js
@@ -0,0 +1,21 @@
+document.getElementById("add-money-btn").addEventListener("click", function(event){
+    event.preventDefault();
+
+    const amount = document.getElementById("amount").value;
+    const convertedAmount = parseInt(amount);
+
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+
+    const currentBalance = document.getElementById("current-balance").innerText;
+    const convertedCurrentBalance = parseFloat(currentBalance)
+    
+    
+    if(convertedPin === 1234){
+        const newAmount = convertedAmount + convertedCurrentBalance;
+        document.getElementById("current-balance").innerText = newAmount;
+    }
+    else{
+        alert("Sorry, something went wrong")
+    }
+})
